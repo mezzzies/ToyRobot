462cb73b475e0d1ca2e8285287d054162ffeb4f9
mezzzies <maytha.c.1996@gmail.com>
2025-02-15 13:43:02 +0700

    UPDATE Test cases UPDATE Invalid value

--- ToyRobot.py
+++ ToyRobot.py
@@ -1,35 +1,56 @@
+class Table:
+    def __init__(self, table_size):
+        self.table_size = table_size
+
 class Robot:
     def __init__(self):
         self.x = None
         self.y = None
         self.facing = None
-        self.table_size = 5
         self.directions = ["NORTH", "EAST", "SOUTH", "WEST"]
         self.placed = False
 
-    def place(self, x, y, facing):
+    def place(self, x, y, facing,table_size):
         try:
-            x, y = int(x), int(y)
-            if 0 <= x <= self.table_size and 0 <= y <= self.table_size and facing in self.directions:
-                self.x = x
-                self.y = y
-                self.facing = facing
-                self.placed = True
+            x, y = float(x), float(y)
+            if not x.is_integer() or not y.is_integer():
+                print("Invalid x or y: floating point not allowed")
+                return
+            else:
+                x, y = int(x), int(y)
+                if 0 <= x <= table_size and 0 <= y <= table_size and facing in self.directions:
+                    self.x = x
+                    self.y = y
+                    self.facing = facing
+                    self.placed = True
+                else:
+                    print("Invalid x or y or facing")
         except ValueError:
-            print("Invalid x or y")
+            print("Invalid x or y: integer expected")
 
-    def move(self):
+    def move(self,table_size):
         if self.x is None or self.y is None:
             return
         
-        if self.facing == "NORTH" and self.y < self.table_size:
+        if self.facing == "NORTH" and self.y < table_size:
             self.y += 1
         elif self.facing == "SOUTH" and self.y > 0:
             self.y -= 1
-        elif self.facing == "EAST" and self.x < self.table_size:
+        elif self.facing == "EAST" and self.x < table_size:
             self.x += 1
         elif self.facing == "WEST" and self.x > 0:
             self.x -= 1
+        else:
+            if self.x == 0 and self.facing == "WEST":
+                print("Robot is at the western edge")
+            elif self.x == table_size and self.facing == "EAST":
+                print("Robot is at the eastern edge")
+            elif self.y == 0 and self.facing == "SOUTH":
+                print("Robot is at the southern edge")
+            elif self.y == table_size and self.facing == "NORTH":
+                print("Robot is at the northern edge")
+            else:
+                print("Invalid move")
 
     def left(self):
         if self.facing:
@@ -50,7 +71,7 @@ class Robot:
 #     print(f"    y = {robot.y}")
 #     print(f"    face = {robot.facing}")
 
-def process_command(robot,text):
+def process_command(robot,table,text):
     parts = text.split(" ",1)
     command = parts[0]
 
@@ -58,15 +79,14 @@ def process_command(robot,text):
         values = parts[1].split(",")  # Split the second part by ","
         if len(values) == 3:
             x,y,face = values
-            robot.place(x,y,face)
+            robot.place(x,y,face,table.table_size)
             #print_status(robot)
-            robot.placed = True
         else:
             print("Invalid format. Expected: PLACE x,y,face")
         
     elif command == "MOVE":
         if robot.placed:
-            robot.move()
+            robot.move(table.table_size)
             #print_status(robot)
         else:
             print("Robot is not placed yet")
@@ -94,13 +114,14 @@ def process_command(robot,text):
 def main():
     try:
         robot = Robot()
+        table = Table(5)
         print("Hi! Welcome to Toy Robot")
         print("Valid commands are: PLACE X,Y,Face | MOVE | LEFT | RIGHT | REPORT")
         print("Note: Place the robot first, Face = [NORTH,EAST,WEST,SOUTH]")
         print("Enter the input or Ctrl+C to exit ...")
         while True:
             line = input().strip()
-            process_command(robot,line)
+            process_command(robot,table,line)
     except EOFError:
         pass  # Handle end of input gracefully
     except KeyboardInterrupt:
--- test_ToyRobot.py
+++ test_ToyRobot.py
@@ -1,72 +1,115 @@
 import pytest
-from ToyRobot import Robot
+from ToyRobot import Table,Robot,process_command
 
+#Unit test cases
+#Positive test cases
+#Fix table_size to 5
 def test_place_robot():
     robot = Robot()
-    robot.place(0, 0, "NORTH")
+    robot.place(0, 0, "NORTH",5)
     assert robot.x == 0
     assert robot.y == 0
     assert robot.facing == "NORTH"
 
 def test_move_robot():
     robot = Robot()
-    robot.place(0, 0, "NORTH")
-    robot.move()
+    robot.place(0, 0, "NORTH",5)
+    robot.move(5)
     assert robot.x == 0
     assert robot.y == 1
 
 def test_turn_left():
     robot = Robot()
-    robot.place(0, 0, "NORTH")
+    robot.place(0, 0, "NORTH",5)
     robot.left()
     assert robot.facing == "WEST"
 
 def test_turn_right():
     robot = Robot()
-    robot.place(0, 0, "NORTH")
+    robot.place(0, 0, "NORTH",5)
     robot.right()
     assert robot.facing == "EAST"
 
 def test_report():
     robot = Robot()
-    robot.place(0, 0, "EAST")
+    robot.place(0, 0, "EAST",5)
     assert robot.report() == "0,0,EAST"
 
-def test_invalid_place():
+#Negative test cases
+
+def test_exceed_place():
     robot = Robot()
-    robot.place(6, 6, "NORTH")
+    robot.place(6, 6, "NORTH",5)
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
+
+def test_float_place():
+    robot = Robot()
+    robot.place(1.4, 1.6, "EAST",5)
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
+
+def test_negative_place():
+    robot = Robot()
+    robot.place(-1, -1, "EAST",5)
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
+
+def test_not_int_xy_place():
+    robot = Robot()
+    robot.place("x", "y", "EAST",5)
     assert robot.x is None
     assert robot.y is None
     assert robot.facing is None
 
 def test_move_out_of_bounds():
     robot = Robot()
-    robot.place(0, 0, "SOUTH")
-    robot.move()
+    robot.place(0, 0, "SOUTH",5)
+    robot.move(5)
     assert robot.y == 0
 
-    robot.place(0, 0, "WEST")
-    robot.move()
+    robot.place(0, 0, "WEST",5)
+    robot.move(5)
     assert robot.x == 0
 
-    robot.place(5, 5, "NORTH")
-    robot.move()
+    robot.place(5, 5, "NORTH",5)
+    robot.move(5)
     assert robot.y == 5
 
-    robot.place(5, 5, "EAST")
-    robot.move()
+    robot.place(5, 5, "EAST",5)
+    robot.move(5)
     assert robot.x == 5
 
 def test_place_invalid_facing():
     robot = Robot()
-    robot.place(0, 0, "UP")
+    robot.place(0, 0, "UP",5)
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
+
+def test_place_space_facing():
+    robot = Robot()
+    robot.place(0, 0, " NORTH",5)
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
+
+    robot.place(0, 0, "NORTH ",5)
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
+
+    robot.place(0, 0, "NO RTH",5)
     assert robot.x is None
     assert robot.y is None
     assert robot.facing is None
 
 def test_move_without_place():
     robot = Robot()
-    robot.move()
+    robot.move(5)
     assert robot.x is None
     assert robot.y is None
 
@@ -83,3 +126,91 @@ def test_turn_right_without_place():
 def test_report_without_place():
     robot = Robot()
     assert robot.report() is None
+
+#Integration test cases
+def test_int_place_robot():
+    robot = Robot()
+    table = Table(5)
+    process_command(robot,table,"PLACE 0,0,EAST")
+    assert robot.x == 0
+    assert robot.y == 0
+    assert robot.facing == "EAST"
+
+def test_int_move_robot():
+    robot = Robot()
+    table = Table(5)
+    process_command(robot,table,"PLACE 0,0,EAST")
+    process_command(robot,table,"MOVE")
+    assert robot.x == 1
+    assert robot.y == 0
+    assert robot.facing == "EAST"
+
+def test_int_turn_robot():
+    robot = Robot()
+    table = Table(5)
+    process_command(robot,table,"PLACE 0,0,EAST")
+    process_command(robot,table,"LEFT")
+    assert robot.x == 0
+    assert robot.y == 0
+    assert robot.facing == "NORTH"
+
+def test_int_report_robot():
+    robot = Robot()
+    table = Table(5)
+    process_command(robot,table,"PLACE 0,0,EAST")
+    process_command(robot,table,"REPORT")
+    assert robot.report() == "0,0,EAST"
+
+#Negative test cases
+def test_place_exceeded_param():
+    robot = Robot()
+    table = Table(5)
+    process_command(robot,table,"PLACE 0,0,EAST,1")
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
+
+def test_place_less_param():
+    robot = Robot()
+    table = Table(5)
+    process_command(robot,table,"PLACE 0,EAST")
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
+
+    process_command(robot,table,"PLACE 0,0")
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
+
+    process_command(robot,table,"PLACE EAST")
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
+
+def test_invalid_place_then_move():
+    robot = Robot()
+    table = Table(5)
+    process_command(robot,table,"0, -1, EAST")
+    process_command(robot,table,"MOVE")
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
+
+def test_invalid_place_then_turn():
+    robot = Robot()
+    table = Table(5)
+    process_command(robot,table,"0, -1, EAST")
+    process_command(robot,table,"LEFT")
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
+    
+def test_invalid_place_then_report():
+    robot = Robot()
+    table = Table(5)
+    process_command(robot,table,"0, -1, EAST")
+    process_command(robot,table,"REPORT")
+    assert robot.x is None
+    assert robot.y is None
+    assert robot.facing is None
